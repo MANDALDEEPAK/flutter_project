c119d776095aa6f505434ac9133b90be86c3e3ae
Your Name <you@example.com>
2025-04-11 17:06:37 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_proj/routes/app_routes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+// void m (){
+//   for(int i = 0; i<=1000000;i++){
+//   }
+//   print('sello world');
+// }
+Future<String> getSome(){
+  return Future.delayed(Duration(seconds: 2),(){
+    // for(int i = 0; i<=1000000;i++){}
+    print('2sec gone');
+    return 'hello world';
+  });
+}
+void a (){
+  print('hello world');
+}
+
 void main(){
+  //m();
+  a();
+  getSome();
   runApp(ProviderScope(child:Home()));
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,45 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_proj/providers/animate.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../app_theme/app_sizes.dart';
 
-class HomePage extends ConsumerWidget {
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context,ref) {
-    final state = ref.watch(animateProvider);
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          AnimatedContainer(
-            height: state.size,
-              width:state.size,
-              color: Colors.orange,
-              duration: Duration(milliseconds: 400),
-          ),
-          AppSizes.gapH16,
-          ElevatedButton(onPressed: (){
-            ref.read(animateProvider.notifier).change(size : state.size + 10);
-          }, child: Text('Click TO Increase')),
-          AppSizes.gapH16,
-          ElevatedButton(onPressed: (){
-            ref.read(animateProvider.notifier).change(size : state.size - 10);
-          }, child: Text('Click To Decrease')),
-          AnimatedOpacity(
-              opacity: state.opacity,
-              duration: Duration(microseconds: 400),
-            child: Image.asset('assets/images/sun-sets.jpg',height: 200,),
-
-          ),
-          ElevatedButton(onPressed: (){
-           ref.read(animateProvider.notifier).change(opacity: 0.5);
-          }, child: Text('Change To Opacity')),
-        ],
-      ),
-    );
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
